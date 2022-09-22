e920bc27aabe8d1c818c7e15d4ddebcca08afa37
dongli0807 <114172379+dongli0807@users.noreply.github.com>
2022-09-23 00:28:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "2bfb45fa8ada2f471b747961cbb50990",
+  APP_SECRET: "8e41eabb95e42c8e4b3b1a5ff783c221",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
